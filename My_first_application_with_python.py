ffb8bcbd0faad4c313dd8d0409b94e5b6aef0b69
Juan David <jcuvidesg1204@gmail.com>
2024-05-29 15:50:32 -0500

    first commit, learn python

---
+++ My_first_application_with_python.py
@@ -0,0 +1,71 @@
+
+print("Hello world!")
+# Print es una funcion
+# Y que es una funcuion?
+# codigo de computadora capas de causar algun efecto o evaluar un valor
+
+print()
+print("Hello my dogs")
+
+# '\' Caracter de escape acompañada de la letra 'n' de newline hacen SALTO DE LINEA 
+
+print("la \ mas la letra n \ncrean un caracter de nueva linea")
+
+# para hacer uso de la \ sin que el interprete ejecute un error usamos \\
+print("\\")
+
+# Para poner mas de un argumento juntos en un mismo print usamos la ','
+
+# Palabras clave: Son usadas para cambiar el comportamiento de la consola
+# end=, se usa para unir dos lineas y agregar algo intre la union.
+# sep=, definir como van a estar separadas las palabras
+
+print("esto es un ejemplo de un uso de end=", end="(separador) " )
+print("continuacion de texto")
+
+print("esto es un ejemplo de un uso de sep", sep="(separador) ")
+print('"Greg\'s book."')
+print("'Greg's book.'")
+
+
+print(3e8)
+print(0.00000000000000000000001)
+print("A mi me gusta\"Monty python\"") 
+print('A mi no me gusta "Monty python"') 
+print('I\'m Monty Python')
+print(4>3)
+print(True>False)
+print(True<False)
+
+# Operaciones basicas en python
+
+# Regla de las operaciones basicas (+, -, *, **, //)
+
+# entero + entero = entero
+# entero + flotante = flotante
+# flotante + entero = flotante
+# flotante + flotante = flotante
+
+#suma
+print(2+2)
+
+#Resta
+print(2-2)
+
+#Exponente (2 elevado a la 3)
+print(2 ** 3)
+
+#multiplicacion
+print(2*3)
+
+#Divicion (El resultado siempre es flotante)
+print(2 / 2)
+
+#Divicion entera
+print(2 // 2)
+
+print(12%4.5)
+
+
+
+
---
+++ Practice_input.py
@@ -0,0 +1,30 @@
+# My firs input with python
+
+# Convertir variables
+
+# numUno = float(input("Escriba un numero "))
+# numDos = float(input("Escriba otro numero "))
+# suma = numUno + numDos
+
+#print(numUno, "+", numDos, "=", suma)
+
+
+# El operador + tambien se usa para concatenar CADENAS de texto
+# nombre = "pepe"
+# apellido = "ramirez"
+# print(nombre + apellido)
+
+# El operador * se usa como multiplicador resibiendo una cadena y un nuemro (cadena * numero), (numero * cadena)
+
+# print(juan * 3)
+
+name = input("Ingresa tu nombre: ")
+print("Hola, " + name + ". ¡Un gusto conocerte!")
+
+x = 11
+y = 4
+
+z = y = x = 1
+print(x, y, z, sep='*')
+
+
